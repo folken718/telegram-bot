8a1c1b33598c2e4d7f6531ba6d231ebd58876f29
J. Manuel Montoya <folken718@gmail.com>
2022-01-12 00:48:31 -0600

    adding exchange rate by date

--- apis/banxico_api.py
+++ apis/banxico_api.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 import requests
+from functools import lru_cache
+from datetime import datetime
+
 
 # Loading .env variables
 load_dotenv()
@@ -11,8 +14,12 @@ TOKEN = os.getenv('BANXICO_TOKEN')
 banxicoUrl = 'https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF60653/datos'
 
 params = {'token': TOKEN}
-exchangeRateRaw = requests.get(banxicoUrl, params=params)
-exchangeRate = exchangeRateRaw.json()
+
+@lru_cache
+def get_exchange_rate_history_baxico():
+    exchangeRateRaw = requests.get(banxicoUrl, params=params)
+    exchangeRate = exchangeRateRaw.json()
+    return exchangeRate;
 
 
 def normilizeData(rawData):
@@ -21,8 +28,10 @@ def normilizeData(rawData):
         dataDictionary[item['fecha']] = item['dato']
     return dataDictionary
 
+def get_exchange_rate_from_date(date):
+    exchangeRate = get_exchange_rate_history_baxico()
+    dataDictionary = normilizeData(exchangeRate['bmx']['series'][0]['datos'])
+    return dataDictionary[date]
 
-rawData = exchangeRate['bmx']['series'][0]['datos']
-goodData = normilizeData(rawData)
-
-print(goodData)
+today = datetime.today().strftime('%d/%m/%Y')
+print(get_exchange_rate_from_date(today))
